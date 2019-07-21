30ecf352dc63cde608c14c7dcadbd397cb20526d
massa142 <arai.masataka142@gmail.com>
2019-07-22 01:16:54 +0900

    画像処理

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,5 +1,8 @@
 import click
 
+from lgtm.drawer import save_with_message
+from lgtm.image_source import get_image
+
 
 @click.command()
 @click.option('--message', '-m', default='LGTM',
@@ -12,4 +15,5 @@ def cli(keyword, message):
 
 
 def lgtm(keyword, message):
-    pass
+    with get_image(keyword) as fp:
+        save_with_message(fp, message)
